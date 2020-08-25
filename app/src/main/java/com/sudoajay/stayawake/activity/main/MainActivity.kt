14e742eefecf04e8fb01e5e28ac759f65619d17d
sudoajay <sudoajay@gmail.com>
2020-08-25 23:59:00 +0530

    Home activity Alomst done

--- app/src/main/java/com/sudoajay/stayawake/activity/main/MainActivity.kt
+++ app/src/main/java/com/sudoajay/stayawake/activity/main/MainActivity.kt
@@ -1,12 +1,61 @@
 package com.sudoajay.stayawake.activity.main
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowManager
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.sudoajay.stayawake.R
 import com.sudoajay.stayawake.activity.BaseActivity
+import com.sudoajay.stayawake.databinding.ActivityMainBinding
+import com.sudoajay.stayawake.helper.DarkModeBottomSheet
 
 class MainActivity : BaseActivity() {
+    private var isDarkTheme: Boolean = false
+    private lateinit var viewModel: MainActivityViewModel
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        isDarkTheme = isDarkMode(applicationContext)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!isDarkTheme)
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        changeStatusBarColor()
+
+        viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+        binding.viewmodel = viewModel
+        binding.lifecycleOwner = this
+
+    }
+
+    private fun showDarkMode() {
+        val darkModeBottomSheet = DarkModeBottomSheet(homeId)
+        darkModeBottomSheet.show(
+            supportFragmentManager.beginTransaction(),
+            darkModeBottomSheet.tag
+        )
+
+    }
+
+    /**
+     * Making notification bar transparent
+     */
+    private fun changeStatusBarColor() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!isDarkTheme) {
+                val window = window
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                window.statusBarColor = Color.TRANSPARENT
+            }
+        }
+    }
+
+    companion object {
+        const val homeId = "home"
     }
 }
---
+++ app/src/main/java/com/sudoajay/stayawake/activity/main/MainActivityViewModel.kt
@@ -0,0 +1,9 @@
+package com.sudoajay.stayawake.activity.main
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainActivityViewModel(application: Application) : AndroidViewModel(application) {
+
+
+}
